1e123068d13f468e3980bfa7c5b7cb029484b3d5
rakesh542000 <pradhanrakeshkumar90@gmail.com>
2026-01-15 12:16:42 +0530

    feat: demonstrate collections and generics with backend use cases

--- src/com/gptclass/javacoreweek1/Collection_GenericsDay_4.java
+++ src/com/gptclass/javacoreweek1/Collection_GenericsDay_4.java
@@ -9,17 +9,13 @@ class Student{
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    
 
     private String name;
 
